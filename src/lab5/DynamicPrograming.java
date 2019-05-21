4e245d1c551b807bba38ba3452c57e53fa66bc41
Gustav <DreamSlayerGX@gmail.com>
2019-05-21 10:01:18 +0200

    Generated a M-array
    M-array that is the cost of movement in the map.
    
    TODO: Find shortest path and do a correct output.

--- src/lab5/DynamicPrograming.java
+++ src/lab5/DynamicPrograming.java
@@ -15,17 +15,54 @@ public class DynamicPrograming {
 	private static int[][] weightTable;
 	private static Pair<String, String>[] queries;
 	
-	private static final int insertCost = -4;
+	private static final int delta = -4;
+	
+	private static final boolean debug = true;
 	
 	public static void main(String[] args) {
 		parseInputData();
-		d_printInput();
-		dynoPro(N-1, N-1);
+		
+		for(Pair<String, String> p : queries) {
+			dynoPro(p.getKey(), p.getValue());
+			
+		}
+		
 		
 	}
 	
-	private static int dynoPro(int i, int k) {
+	private static int dynoPro(String a, String b) {
+		int[][] M = generateMArray(a,b);
+		
+		
 		 return 0;
+	}
+	
+	private static int[][] generateMArray(String a, String b){
+		int[][] M = new int[a.length() + 1][b.length() + 1];
+		
+		int loopLength = Math.max(a.length(), b.length());
+		for(int i = 0; i <= loopLength; i++) {
+			if(i <= a.length())
+				M[i][0] = i*delta;
+			
+			if(i <= b.length())
+				M[0][i] = i*delta;
+			
+		}
+		
+		for(int i = 1; i <= a.length(); i++)
+			for(int j = 1; j <= b.length(); j++) {
+				int x = M[i-1][j-1] + weightTable[characters.get(String.valueOf(a.charAt(i-1)))][characters.get(String.valueOf(b.charAt(j-1)))];
+				int y = M[i-1][j] + delta;
+				int z = M[i][j - 1] + delta;
+				int midMax = Math.max(x, y);
+				
+				M[i][j] = Math.max(midMax, z);
+			}
+		
+		d_printMArray(M, a, b);
+		
+		return M;
 	}
 	
 	private static void parseInputData() {
@@ -55,7 +92,8 @@ public class DynamicPrograming {
 				String[] queryRow = sc.nextLine().split(" ");
 				queries[i] = new Pair<String, String>(queryRow[0], queryRow[1]);
 			}
-				
+			
+			d_printInput();
 				
 			sc.close();
 		} catch (Exception e) {
@@ -63,7 +101,23 @@ public class DynamicPrograming {
 		}	
 	}
 	
+	private static void d_printMArray(int[][] M, String a, String b) {
+		if(!debug)
+			return;
+		System.out.println("\n");
+		System.out.println(a + "  " + b);
+		for(int i = a.length(); i >= 0 ; i--) {
+			for(int j = 0; j <= b.length(); j++)
+				System.out.print(M[i][j] + "\t");
+			System.out.println();
+		}
+		
+	}
+	
 	private static void d_printInput() {
+		if(!debug)
+			return;
+		
 		for(Entry s : characters.entrySet())
 			System.out.print(s.getKey() + " ");
 		
@@ -82,3 +136,13 @@ public class DynamicPrograming {
 	}
 
 }
+
+/*
+ * 	if(isCached(...)) {
+    	return getCache(...);
+	}
+	
+	int cost = calculate(...);
+	setCache(cost, ...);
+	return cost;
+ */
