2b4980042a3a3e6a94ba87f89ae6a1ff7db8b97d
Gustav <DreamSlayerGX@gmail.com>
2019-05-26 13:08:54 +0200

    Subset and Graph
    Added functions to create a sub set of edges and make that subset into a graph that will be used to iterate over with DFS.

--- src/lab6/NetworkFlow.java
+++ src/lab6/NetworkFlow.java
@@ -1,6 +1,11 @@
 package lab6;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class NetworkFlow {
@@ -17,17 +22,23 @@ public class NetworkFlow {
 			this.weigt = weigt;
 		}
 		
+		public String toString() {
+			return (nodeA) + " " + (nodeB) + " " + (weigt);
+		}
+		
 	}
 	
 	private static int N, M, C, P;
-	private static int[] removeEdge;
+	private static ArrayList<Integer> removeEdge;
 	private static Edge[] edges;
+	
+	private static Map<Integer, ArrayList<Integer>> graph;
 	
 	private static int leftBound, rightBound;
 	private static int binaryIndex;
 	
 	//Change this to false to stop debug (d_ methods) prints!
-	private static final boolean debug = false;
+	private static final boolean debug = true;
 	
 	public static void main(String[] args) {
 		parseInputData();
@@ -36,9 +47,12 @@ public class NetworkFlow {
 	
 	private static void fordFulkerson() {
 		
-		int i = 0;
+		int i = -1;
 		int flow = 0;
-		while(i++ < 10) {
+		
+		while(i++ != 3) {
+		//while(i != binaryIndex) {
+			//i = binaryIndex;
 			setNextBinaryIndex(flow);
 			flow = DFS();
 			
@@ -48,6 +62,72 @@ public class NetworkFlow {
 		
 	}
 	
+	private static int DFS() {
+		Edge[] edgeSubSet = generateSubSet();
+		boolean[] visited = new boolean[edgeSubSet.length];
+
+		if(edgeSubSet == null)
+			return 0;
+		
+		
+		return 0;
+	}
+	
+	/*
+	 * To generate the subset of edges to go through.
+	 * This will take O(N) since we go through the list every time. Save list and modify it?
+	 */
+	private static Edge[] generateSubSet() {
+		Edge[] subSet = new Edge[(edges.length - 1) - binaryIndex];
+		List<Integer> subRemove = removeEdge.subList(0, binaryIndex + 1);
+		int j = 0;
+		
+		for(int i = 0; i < edges.length; i++) {
+			if(!subRemove.contains(i))
+				subSet[j++] = edges[i]; 
+		}
+		
+		d_printSubSet(subSet);
+		
+		return createNewGraph(subSet) ? subSet : null;
+	}
+	
+	/*
+	* Generate the graph corresponding to the subset of edges.
+	* Costly to do this every time. Returns false if node Minsk (0) or Lund (N-1) doesn't exist.
+	* O(N^2)
+	*/
+	private static boolean createNewGraph(Edge[] edgeSubSet) {
+		graph = new HashMap<>();
+		
+		for(int i = 0; i < edgeSubSet.length; i++) {
+			int keyNode = edgeSubSet[i].nodeA;
+			
+			if(!graph.containsKey(keyNode))
+				graph.put(keyNode, new ArrayList<>());
+			
+			graph.get(keyNode).add(edgeSubSet[i].nodeB);
+			
+			for(int j = i + 1; j < edgeSubSet.length; j++) {
+				Edge e = edgeSubSet[j];
+				
+				if(e.nodeA == keyNode)
+					graph.get(keyNode).add(e.nodeB);
+				else if(e.nodeB == keyNode)
+					graph.get(keyNode).add(e.nodeA);
+				
+			}
+		}
+		
+		d_printGraph();
+		
+		return (graph.containsKey(0) || graph.containsKey(N - 1));
+		
+	}
+	
+	/*
+	 * To set the index of which we remove edges (binary search)
+	 */
 	private static void setNextBinaryIndex(int flow) {
 		
 		binaryIndex = (rightBound + leftBound)/2;
@@ -56,11 +136,6 @@ public class NetworkFlow {
 			rightBound = binaryIndex - 1;
 		else if(flow > C) 
 			leftBound = binaryIndex + 1;
-	}
-	
-	private static int DFS() {
-		
-		return 0;
 	}
 	
 	private static void parseInputData() {
@@ -81,13 +156,38 @@ public class NetworkFlow {
 			edges[i] = new Edge(edgeInfo[0], edgeInfo[1], edgeInfo[2]);
 		}
 		
-		removeEdge = new int[P];
+		removeEdge = new ArrayList<>(P);
 		for(int i = 0; i < P; i++) {
-			removeEdge[i] = Integer.parseInt(sc.nextLine());
+			removeEdge.add(Integer.parseInt(sc.nextLine()));
 		}
 		
 		sc.close();
 		d_printInputData();
+	}
+	
+	private static void d_printGraph() {
+		if(!debug)
+			return;
+		
+		System.out.print("\n\nGraph");
+		for(Entry<Integer, ArrayList<Integer>> set : graph.entrySet()) {
+			System.out.print("\n" + set.getKey() + " | ");
+			for(Integer toNode : set.getValue())
+				System.out.print(" " + toNode);
+			
+		}
+	}
+	
+	private static void d_printSubSet(Edge[] subSet) {
+		System.out.println("\n\nSubSet");
+		System.out.println("BinaryIndex = " + binaryIndex);
+		for(Edge e : subSet) {
+			if(e == null) {
+				System.out.println("NULL");
+			} else {
+				System.out.println(e.toString());
+			}
+		}
 	}
 	
 	private static void d_printInputData() {
@@ -97,7 +197,7 @@ public class NetworkFlow {
 		System.out.println(N + " " + M + " " + C + " " + P);
 		
 		for(Edge e : edges) {
-			System.out.println(e.nodeA + " " + e.nodeB + " " + e.weigt);
+			System.out.println(e.toString());
 		}
 		
 		for(int i : removeEdge) {
